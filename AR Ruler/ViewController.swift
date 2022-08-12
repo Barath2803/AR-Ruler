899f407d5bc6698a7b269a7ddd2bff92ba7e4e92
Barath2803 <1934004aiml@cit.edu.in>
2022-08-12 15:31:21 +0530

    final commit code

--- AR Ruler/ViewController.swift
+++ AR Ruler/ViewController.swift
@@ -10,7 +10,11 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    var pointNodes = [SCNNode]()
+    var textNode = SCNNode()
 
+    @IBOutlet weak var distanceLabel: UILabel!
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
@@ -19,14 +23,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        sceneView.debugOptions = [SCNDebugOptions.showFeaturePoints]
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -46,29 +44,82 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
+    
+    func addPoint(withLocation location: ARHitTestResult){
+        let point = SCNSphere(radius: 0.003)
+        
+        let getMaterial = SCNMaterial()
+        getMaterial.diffuse.contents = UIColor.red
+        
+        point.materials = [getMaterial]
+        
+        let pointNode = SCNNode(geometry: point)
+        
+        pointNode.position = SCNVector3(
+            x: location.worldTransform.columns.3.x,
+            y: location.worldTransform.columns.3.y,
+            z: location.worldTransform.columns.3.z)
+
+        sceneView.scene.rootNode.addChildNode(pointNode)
+
+        pointNodes.append(pointNode)
+        
+        if pointNodes.count >= 2 {
+            calculate()
+        }
+    }
+    
+    func calculate() {
+        let start = pointNodes[0]
+        let end = pointNodes[1]
+        
+        let distance = sqrt(
+            pow(end.position.x - start.position.x, 2) +
+            pow(end.position.y - start.position.y, 2) +
+            pow(end.position.z - start.position.z, 2)
+        )
+        
+        addText(text: "\(abs(distance*100))", atPosition: end.position)
+         
+        distanceLabel.text = "Distance: \(abs(distance*100))cm"
+    }
+    
+    func addText(text: String, atPosition position: SCNVector3) {
+        let textGeometry = SCNText(string: text, extrusionDepth: 1.0)
+        
+        textGeometry.firstMaterial?.diffuse.contents = UIColor.red
+        
+        textNode = SCNNode(geometry: textGeometry)
+        
+        textNode.position = SCNVector3(x: position.x, y: position.y + 0.05, z: position.z)
+        
+        textNode.scale = SCNVector3(x: 0.01, y: 0.01, z: 0.01)
+    
+        sceneView.scene.rootNode.addChildNode(textNode)
+    }
+    
     // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
+        if pointNodes.count >= 2 {
+            for points in pointNodes {
+                points.removeFromParentNode()
+                textNode.removeFromParentNode()
+            }
+            pointNodes = [SCNNode]()
+        }
+        
+        if let touch = touches.first {
+            let touchLocation = touch.location(in: sceneView)
+            
+            let result = sceneView.hitTest(touchLocation, types: .featurePoint)
+            
+            if let hitResult = result.first {
+                addPoint(withLocation: hitResult)
+            }
+        }
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
+ 
